4f12cb1a188babf6ab71237ea5b6090f26c9a823
HYPERPC <serj5001@yandex.ru>
2025-04-18 18:27:16 +0300

    15 st

---
+++ src/main/kotlin/ru/shopkotlin/sring_kotlin/SringKotlinApplication.kt
@@ -0,0 +1,11 @@
+package ru.shopkotlin.sring_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SringKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SringKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/ru/shopkotlin/sring_kotlin/SringKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.shopkotlin.sring_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SringKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
