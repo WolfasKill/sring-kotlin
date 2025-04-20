a47800256195cb68577aa7262e16764d4e92edca
HYPERPC <serj5001@yandex.ru>
2025-04-20 13:14:07 +0300

    20 New Target

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS TBL_PRODUCT;
-
+DROP TABLE IF EXISTS TBL_PRODUCT_NEW;
+DROP TABLE IF EXISTS TBL_GRP_PRODUCT;
+DROP TABLE IF EXISTS TBL_PRICE;
+DROP TABLE IF EXISTS TBL_PRICE_TYPE;
 
 
 
@@ -11,7 +14,35 @@ CREATE TABLE TBL_PRODUCT (
 
 
 
+
+
+CREATE TABLE TBL_GRP_PRODUCT (
+                                 sid INT AUTO_INCREMENT PRIMARY KEY,
+                                 pid INT,
+                                 hc boolean,
+                                 name VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE TBL_PRODUCT_NEW (
+                             id INT AUTO_INCREMENT PRIMARY KEY,
+                             name VARCHAR(250) NOT NULL,
+                             sid INT NOT NULL
+);
+
+CREATE TABLE TBL_PRICE (
+                           p_id INT AUTO_INCREMENT PRIMARY KEY,
+                           id INT NOT NULL,
+                           p_t_id INT NOT NULL,
+                           price INT NOT NULL
+);
+
+CREATE TABLE TBL_PRICE_TYPE (
+                                p_t_id INT AUTO_INCREMENT PRIMARY KEY,
+                                name VARCHAR(250) NOT NULL
+);
+
 commit;
+
 
 
 
